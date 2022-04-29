684c25e522f546a4a0face4f5b4dbd3f4415c0c2
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-04-29 22:09:37 +0300

    init

---
+++ L01Syntax, Functions and Statements/Lab/T01echoFunc.js
@@ -0,0 +1,6 @@
+    function t01echoFunc(str) {
+        console.log(str.length);
+        console.log(str)
+    }
+
+t01echoFunc("Hello WOrld");
---
+++ L01Syntax, Functions and Statements/Lab/T02strLen.js
@@ -0,0 +1,7 @@
+function t02strLen(str1,str2,str3) {
+    let sum = str1.length + str2.length + str3.length;
+    console.log(sum);
+    console.log(Math.floor(sum / 3));
+}
+
+t02strLen('chocolate', 'ice cream   ', 'cake');
