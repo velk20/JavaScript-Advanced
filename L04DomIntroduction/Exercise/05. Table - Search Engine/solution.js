e4387e5dde86861a2b9ad5959f460c9e15a49160
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-05-28 22:50:01 +0300

    exercise

--- L04DomIntroduction/Exercise/05. Table - Search Engine/solution.js
+++ L04DomIntroduction/Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,24 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const searchWord = document.getElementById('searchField').value;
+      document.getElementById('searchField').value = '';
+
+
+      const items = Array.from(document.querySelectorAll('tbody tr'));
+      items.map(e => e.classList.remove('select'));
+
+      console.log(items);
+      for (const item of items) {
+         let [...arr] = Array.from(item.children);
+         for (const element of arr) {
+            if (element.textContent.includes(searchWord)) {
+               item.classList.add("select");
+            }
+         }
+      }
+
+
 
    }
 }
