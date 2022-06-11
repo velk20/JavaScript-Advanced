956049333b88bee2d7dabee5dc1936efe8de5b56
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-06-11 20:50:03 +0300

    fds

--- L07UnitTestinAndErrorHandling/Lab/test.js
+++ L07UnitTestinAndErrorHandling/Lab/test.js
@@ -1,5 +1,12 @@
+const { assert, expect } = require('chai');
+const { sum, product } = require('../Lab/MyModule');
+
 describe('Test Suite', () => {
-  it('test 1', () => {
-    console.log('first test');
+  it('works with positive integers', () => {
+    assert.equal(sum(3, 5), 8, 'did not work with 3 and 5');
+  });
+
+  it('works with positive floats', () => {
+    expect(sum(3, 3)).to.equal(6);
   });
 });
