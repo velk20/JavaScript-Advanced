636f0ec684cdfe39bd14200687912d8bd1a3ed1e
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-06-11 22:11:26 +0300

    test

---
+++ L07UnitTestinAndErrorHandling/Lab/T01SubSum.js
@@ -0,0 +1,19 @@
+function subSum(arr, startIndex, endIndex) {
+  if (!Array.isArray(arr)) return NaN;
+  if (startIndex < 0) startIndex = 0;
+  if (endIndex > arr.length - 1) endIndex = arr.length - 1;
+
+  let sum = 0;
+  for (let index = startIndex; index <= endIndex; index++) {
+    const element = arr[index];
+    sum += Number(element);
+  }
+
+  return sum;
+}
+
+console.log(subSum([10, 20, 30, 40, 50, 60], 3, 300));
+console.log(subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1));
+console.log(subSum([10, 'twenty', 30, 40], 0, 2));
+console.log(subSum([], 1, 2));
+console.log(subSum('text', 0, 2));
---
+++ L07UnitTestinAndErrorHandling/Lab/tests.test.js
@@ -0,0 +1,63 @@
+const { assert, expect } = require('chai');
+const { sum } = require('../Lab/T04SumOfNumber');
+const { isSymmetric } = require('../Lab/T05CheckForSymmetry');
+
+/*
+describe('Test Suite', () => {
+  it('works with positive integers', () => {
+    assert.equal(sum(3, 5), 8, 'did not work with 3 and 5');
+  });
+
+  it('works with positive floats', () => {
+    expect(sum(3, 3)).to.equal(6);
+  });
+});*/
+
+//* Task 4
+/*
+describe('Test Sum of Numbers', () => {
+  it('Check if positive numbers', () => {
+    expect(sum([1, 2, 3, 2])).to.be.equal(8);
+  });
+
+  it('Check zero on empty array', () => {
+    expect(sum([])).to.be.equal(0);
+  });
+
+  it('Check if positive numbers', () => {
+    expect(sum([1, 2, 3, '2'])).to.be.equal(8);
+  });
+
+  it('Check if positive numbers', () => {
+    expect(sum([1, 2, 3, 'a'])).to.be.NaN;
+  });
+});*/
+
+//*Task 5
+describe('Test Sum of Numbers', () => {
+  it('returns true for symmetric array', () => {
+    expect(isSymmetric([1, 2, 2, 1])).to.be.true;
+  });
+  it('returns false for non-symmetric array', () => {
+    expect(isSymmetric([1, 2, 3])).to.be.false;
+  });
+  it('returns false for non-array', () => {
+    expect(isSymmetric(5)).to.be.false;
+  });
+  it('returns false for type-different symmetric array', () => {
+    expect(isSymmetric([1, 2, '1'])).to.be.false;
+  });
+  //Our bonus tests
+  it('returns true for symmetric array with odd numbers of elements', () => {
+    expect(isSymmetric([1, 2, 1])).to.be.true;
+  });
+  it('returns true for symmetric array with strings', () => {
+    expect(isSymmetric(['a', 'b', 'b', 'a'])).to.be.true;
+  });
+  it('returns false for non-symmetric array with strings', () => {
+    expect(isSymmetric(['a', 'b', 'c'])).to.be.false;
+  });
+  it('returns false for string parameter', () => {
+    expect(isSymmetric('abba')).to.be.false;
+  });
+});
