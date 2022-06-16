901232763dd4aa8298b09c8c2f92e9e70e183044
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-06-16 11:00:01 +0300

    update examPrep

--- L10ExamPrep/JSAdvancedExam13March2022/carDealership.js
+++ L10ExamPrep/JSAdvancedExam13March2022/carDealership.js
@@ -36,7 +36,7 @@ class CarDealership {
 
     return `New car added: ${model} - ${horsepower} HP - ${mileage.toFixed(
       2
-    )} km - ${price.toFixed(2)}$`;
+    )} km - ${price.toFixed(2)}$`.trim();
   }
 
   sellCar(model, desiredMileage) {
@@ -46,12 +46,15 @@ class CarDealership {
       throw new Error(`${model} was not found!`);
     }
 
-    if (currentCar.mileage - desiredMileage <= 40000) {
-      currentCar.price *= 0.95;
-    }
+    if (currentCar.mileage <= desiredMileage) {
+    } else {
+      if (Math.abs(currentCar.mileage - desiredMileage) <= 40000) {
+        currentCar.price *= 0.95;
+      }
 
-    if (currentCar.mileage - desiredMileage > 40000) {
-      currentCar.price *= 0.9;
+      if (Math.abs(currentCar.mileage - desiredMileage) > 40000) {
+        currentCar.price *= 0.9;
+      }
     }
 
     this.availableCars.splice(this.availableCars.indexOf(currentCar), 1);
@@ -63,7 +66,7 @@ class CarDealership {
 
     this.totalIncome += currentCar.price;
 
-    return `${model} was sold for ${currentCar.price.toFixed(2)}$`;
+    return `${model} was sold for ${currentCar.price.toFixed(2)}$`.trim();
   }
 
   currentCar() {
@@ -74,10 +77,10 @@ class CarDealership {
     for (const car of this.availableCars) {
       res += `---${car.model} - ${car.horsepower} HP - ${car.mileage.toFixed(
         2
-      )} km - ${car.price.toFixed(2)}$`;
+      )} km - ${car.price.toFixed(2)}$\n`;
     }
 
-    return res;
+    return res.trim();
   }
 
   salesReport(criteria) {
@@ -102,10 +105,12 @@ class CarDealership {
         } HP - ${car.soldPrice.toFixed(2)}$\n`)
     );
 
-    return res;
+    return res.trim();
   }
 }
 let dealership = new CarDealership('SoftAuto');
-console.log(dealership.addCar('Toyota Corolla', 100, 3500, 190000));
-console.log(dealership.addCar('Mercedes C63', 300, 29000, 187000));
-console.log(dealership.addCar('', 120, 4900, 240000));
+dealership.addCar('Toyota Corolla', 100, 3500, 190000);
+dealership.addCar('Mercedes C63', 300, 29000, 187000);
+dealership.addCar('Audi A3', 120, 4900, 240000);
+console.log(dealership.sellCar('Toyota Corolla', 230000));
+console.log(dealership.sellCar('Mercedes C63', 110000));
