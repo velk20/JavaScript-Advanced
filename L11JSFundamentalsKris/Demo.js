30e40febcc731be64a5cac7bb651726bde5908ef
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-06-21 18:34:16 +0300

    kr

---
+++ L11JSFundamentalsKris/Demo.js
@@ -0,0 +1,16 @@
+//reference types
+
+function qqq(num) {
+  if (typeof num == 'number') {
+    return 'hi number';
+  }
+  return 'hi baby!';
+}
+let namee = qqq('saas');
+console.log(namee);
+
+let num = (a, b) => {
+  sdasdasdd;
+  rett;
+};
+num(2, 2);
