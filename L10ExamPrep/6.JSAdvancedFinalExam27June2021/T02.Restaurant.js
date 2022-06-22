ff9a1b8e396112f1494ec64fa3ac7f98e6933a5c
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-06-22 15:26:55 +0300

    -

---
+++ L10ExamPrep/6.JSAdvancedFinalExam27June2021/T02.Restaurant.js
@@ -0,0 +1,14 @@
+class Restaurant {
+  constructor(budget) {
+    this.budgetMoney = Number(budget);
+    this.menu = {};
+    this.stockProducts = {};
+    this.history = [];
+  }
+
+  loadProducts(products) {
+    for (const product of products) {
+      let [name, quantity, totalPrice] = product.split(' ');
+    }
+  }
+}
